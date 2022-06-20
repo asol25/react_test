c09a80a35dc31c94d66c0f57f8222c112a797bde
asol25 <usool.203@gmail.com>
2022-06-20 16:36:36 +0700

    fix job_Service

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React, { useEffect } from 'react'
 import { Main } from './components/MainlLayout/MainSection/Main'
 import { Job } from './assets/type';
-import movie_Service from './assets/movie-service';
+import job_Service from './assets/movie-service';
 
 const App: React.FC = () => {
   const [job, setJob] = React.useState<Job[]>([]);
   const [value, setValue] = React.useState<number>(0);
   useEffect(() => {
-    movie_Service().then(function (response) {
+    job_Service().then(function (response) {
       setJob(response);
     })
   }, []);
